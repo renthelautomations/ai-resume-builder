51dd8934a4459ea5431d1595207d8fca33eb7b19
renthelautomations <rnthlcueto@gmail.com>
2026-07-04 23:15:16 +0800

    Configure Vercel Node runtime for Express

--- server.js
+++ server.js
@@ -41,6 +41,10 @@ app.post('/api/generate', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = app;
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
